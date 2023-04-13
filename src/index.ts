020c8517281fb0d93e9acc1a200dc628eb14571e
Luis Espinoza <luis@stakeordie.com>
2023-04-13 16:58:41 -0700

    Update @stakeordie/emprops-core to 0.1.5

--- src/index.ts
+++ src/index.ts
@@ -19,31 +19,29 @@ const artGen = new ArtGen({
   openai: {
     apiKey: process.env.OPENAI_API_KEY,
   },
-});
-
-artGen.onNewToken(async (token, api) => {
-  const rng = new PseudoRandom(token.seed);
-  const seed = rng.pseudorandomInteger(1000000000, 4294967295);
-  const temperature = parseFloat(rng.pseudorandom(0, 0.65).toFixed(1));
-  const setting = await api.runOpenAICompletion({
-    model: "text-davinci-003",
-    prompt: "Suggest a setting for a photo",
-    temperature,
-    n: 1
-  });
-  let output = await api.runSd({
-    api: "txt2img",
-    prompt: `A photo of a ${setting}, Realistic`,
-    negative_prompt:
-      "((letters)), ((numbers)), ((text)), ((symbols)), ((sentences)), ((paragraphs)), ((web interface)), ((gui)), ((web app)), ((desktop app))",
-    seed,
-    sampler_name: "DPM++ SDE",
-  });
-  output = await api.runSdUpscaler({
-    image: output,
-    upscaler_1: "R-ESRGAN 4x+",
-  });
-  return output;
-});
-
-artGen.start(10);
+})
+  .onNewToken(async (token, api) => {
+    const rng = new PseudoRandom(token.seed);
+    const seed = rng.pseudorandomInteger(1000000000, 4294967295);
+    const temperature = parseFloat(rng.pseudorandom(0, 0.65).toFixed(1));
+    const setting = await api.runOpenAICompletion({
+      model: "text-davinci-003",
+      prompt: "Suggest a setting for a photo",
+      temperature,
+      n: 1,
+    });
+    let output = await api.runSd({
+      api: "txt2img",
+      prompt: `A photo of a ${setting}, Realistic`,
+      negative_prompt:
+        "((letters)), ((numbers)), ((text)), ((symbols)), ((sentences)), ((paragraphs)), ((web interface)), ((gui)), ((web app)), ((desktop app))",
+      seed,
+      sampler_name: "DPM++ SDE",
+    });
+    output = await api.runSdUpscaler({
+      image: output,
+      upscaler_1: "R-ESRGAN 4x+",
+    });
+    return output;
+  })
+  .start(10);
