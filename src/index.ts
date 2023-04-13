9346ac7daca8f96b4721d1113387efd76393f45a
Luis Espinoza <luis@stakeordie.com>
2023-04-13 16:53:55 -0700

    Update @stakeordie/emprops-core to 0.1.4

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import dotenv from "dotenv";
-import { createArtGen, PseudoRandom } from "@stakeordie/emprops-core";
+import { ArtGen, PseudoRandom } from "@stakeordie/emprops-core";
 
 dotenv.config();
 
-const artGen = createArtGen({
+const artGen = new ArtGen({
   debug: process.env.DEBUG === "true",
-  storage: {
+  output: {
     // If you want to change this, make sure you update the "setup" script in package.json
     path: "./outputs",
   },
@@ -23,25 +23,27 @@ const artGen = createArtGen({
 
 artGen.onNewToken(async (token, api) => {
   const rng = new PseudoRandom(token.seed);
-  const seed = rng.pseudorandomInteger(0, 4294967295);
+  const seed = rng.pseudorandomInteger(1000000000, 4294967295);
+  const temperature = parseFloat(rng.pseudorandom(0, 0.65).toFixed(1));
   const setting = await api.runOpenAICompletion({
     model: "text-davinci-003",
     prompt: "Suggest a setting for a photo",
-    temperature: 0.8,
+    temperature,
+    n: 1
   });
   let output = await api.runSd({
     api: "txt2img",
-    prompt: `A photo of a person, ${setting}`,
-    negativePrompt:
+    prompt: `A photo of a ${setting}, Realistic`,
+    negative_prompt:
       "((letters)), ((numbers)), ((text)), ((symbols)), ((sentences)), ((paragraphs)), ((web interface)), ((gui)), ((web app)), ((desktop app))",
     seed,
-    smaplerName: "Euler",
+    sampler_name: "DPM++ SDE",
   });
   output = await api.runSdUpscaler({
     image: output,
-    upscaler1: "R-ESRGAN 4x+",
+    upscaler_1: "R-ESRGAN 4x+",
   });
   return output;
 });
 
-artGen.start(2);
+artGen.start(10);
